6547bb748325f294615de44fb9481cb61486d1c9
marvcoolguy <86561662+marvcoolguy@users.noreply.github.com>
2021-12-05 08:27:03 +0100

    Dashboard

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-
-let word = text => document.getElementById("text").innerHTML = `Hello ${text}`;
-let text = prompt("what is your name");
-word(text);
+let text = prompt("What is your name");
+if (text == "1Marvelous Martins-Abel") {
+    document.getElementById("text").innerHTML = `Welcome my master and my creator`;
+}
+else if (text == "I am an idiot" || text == "") {
+    document.getElementById("text").innerHTML = `Your an idiot to think that will work on me`;
+}
+else if (!(isFinite(text))) {
+    document.getElementById("text").innerHTML = `Hello ${text}`;
+} else {
+    document.getElementById("text").innerHTML = "You didn't type your name please reload and type your name "
+}
